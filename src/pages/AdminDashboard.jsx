c37b6da8ce6b7458552d895912016222c77a4c10
Tynneth Jhoel Fuentes <pheonix0014@gmail.com>
2023-08-06 08:19:38 +0800

    initial admin-dashboard

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+function AdminDashboard() {
+  return (
+    <div className="bg-gray-50_02 flex sm:flex-col md:flex-col flex-row font-gilroy gap-[30px] items-start mx-auto sm:pr-5 pr-6 w-full">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default AdminDashboard;
