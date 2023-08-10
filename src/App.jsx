ae631e4fac6fbc8c20b253259788efe5291e78a4
Mao <ongmarkanthony@gmail.com>
2023-08-11 00:19:44 +0800

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import LoginPage from "../components/LoginPage"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import "./index.css";
 import UserDashboard from "./components/userDashboard";
 import UserProfile from "./components/UserProfile";
@@ -13,17 +13,17 @@ const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        {loggedIn && (
         <Route path="/UserDashboard" element={<UserDashboard />} />
         <Route path="/UserProfile" element={<UserProfile />} />
         <Route path="/BundyClock" element={<BundyClock />} />
         <Route path="/TimeOff" element={<TimeOff />} />
         <Route path="/Document" element={<Documents />} />
       </Routes>
-    </Router>
-  );}
+    </BrowserRouter>
+  );
+};
 
 export default App;
