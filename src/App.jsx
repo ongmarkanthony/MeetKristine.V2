9b8a275f218e1c140d584c836af377e8d0df85d5
Mao <ongmarkanthony@gmail.com>
2023-08-11 00:24:57 +0800

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import LoginPage from "../components/LoginPage"
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import LoginPage from "../pages/LoginPage"
+import { BrowserRouter as  Route, Routes, } from 'react-router-dom';
 import "./index.css";
-import UserDashboard from "./components/userDashboard";
+import UserDashboardPages from "./pages/userDashboardPages";
 import UserProfile from "./pages/UserProfile";
 import TimeOff from "./pages/TimeOff";
 import Documents from "../components/Documents";
@@ -10,13 +10,12 @@ import BundyClock from './pages/bundyClock';
 
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/UserDashboard" element={<UserDashboard />} />
+        <Route path="/UserDashboardPages" element={<UserDashboardPages />} />
         <Route path="/UserProfile" element={<UserProfile />} />
         <Route path="/BundyClock" element={<BundyClock />} />
         <Route path="/TimeOff" element={<TimeOff />} />
