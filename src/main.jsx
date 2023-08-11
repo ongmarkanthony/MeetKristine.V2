b1cfceeee56d9b083c1dba9d38f8afbe4dba1d0a
Mao <ongmarkanthony@gmail.com>
2023-08-11 11:58:03 +0800

    accept changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import "./index.css";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-=======
 import React from 'react';
 import './index.css'
 import { createRoot } from 'react-dom';  
 import App from './App';
->>>>>>> 79cc6c59390535015a4f79fefd51f7c06243f289
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
