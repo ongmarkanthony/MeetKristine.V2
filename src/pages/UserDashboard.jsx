0f905a992e93b4221c09c13a8b0d32103679de0f
Mao <ongmarkanthony@gmail.com>
2023-08-17 02:52:33 +0800

    update userDB

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import UserFooter from '../components/UserFooter';
-import TaskBar from '../components/TaskBar';
-import TimeChart from '../components/TimeChart';
 import MainSidebar from '../components/GlobalSidebar';
 import UserDBContent from '../components/UserDBContent';
 
 const UserDashboard = () => {
-  const [holidays, setHolidays] = useState([]);
-
-  useEffect(() => {
-    const fetchHolidays = async () => {
-      try {
-        const response = await fetch("https://holidayapi.com/v1/holidays?pretty&key=8e2addfa-5dc8-49e0-890f-467a7c128dbe&country=PH&year=2022");
-        const data = await response.json();
-        setHolidays(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchHolidays  }, []);
 
   return (
     <>
