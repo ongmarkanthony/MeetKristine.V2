fb2a455db3037298fd8d6b88ace3fbb14f36846b
Mao <ongmarkanthony@gmail.com>
2023-08-22 23:08:43 +0800

    update

--- src/pages/Management.jsx
+++ src/pages/Management.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import CardContainer from "../components/CardContainer";
-import Sidebar from "../components/Sidebar";
+import NavLink from "../components/NavLink";
 
 const Management = () => {
   return (
     <div className="flex items-center justify-end max-w-1268px mx-auto md:px-5 w-full">
       <div>
-        <Sidebar />
+        <NavLink />
       </div>
       <div className="flex flex-col gap-30px items-start justify-start w-full">
         <p className="font-medium text-2xl md:text-22px text-black-900_01 sm:text-xl">
